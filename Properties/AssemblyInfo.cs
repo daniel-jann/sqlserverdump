3bfde962a33dd76ed8293eaac5e2f9c08e5348fb
helvartis <devnull@localhost>
2015-04-29 12:28:09 +0200

    Updated assembly info (copyright date and file and assembly version)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Helvartis")]
 [assembly: AssemblyProduct("sqlserverdump")]
-[assembly: AssemblyCopyright("Copyright © Helvartis 2011-2013")]
+[assembly: AssemblyCopyright("Copyright © Helvartis 2011-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,6 +33,6 @@ using System.Resources;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.0.0")]
-[assembly: AssemblyFileVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.1.0")]
+[assembly: AssemblyFileVersion("1.5.1.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
